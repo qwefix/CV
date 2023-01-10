d63a31b731b935d22beb0ac82b8542692272569c
Alexey Hramchenko <70804795+qwefix@users.noreply.github.com>
2023-01-10 15:08:12 +0300

    clear

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   React.useEffect(() => {
@@ -9,24 +7,8 @@ function App() {
       .then((data) => console.log(data));
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>  
+    Hi  hi</div>;
 }
 
 export default App;
